a4642fa26ca630c497df5bf60fe5dadf9e02c5a3
DANIEL ARTURO DE LA TORRE CAMPOS <I20050011@monclova.tecnm.mx>
2020-09-14 17:25:58 -0500

    a

---
+++ github/github/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola ¿que tal?");
+            Console.WriteLine("hoy es 14 de septiembre");
+            Console.ReadKey();
+               
+        }
+    }
+}
